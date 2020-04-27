9860830295b93c5d3ad5c147605bb53800599b6b
Mecca Adams <meccaadams88@gmail.com>
2020-04-26 20:54:33 -0400

    Add Heart Icon

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,63 +1,103 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { View, Platform } from "react-native";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
   {
     Directory: { screen: Directory },
-    CampsiteInfo: { screen: CampsiteInfo }
+    CampsiteInfo: { screen: CampsiteInfo },
   },
   {
-    initialRouteName: 'Directory',
+    initialRouteName: "Directory",
     navigationOptions: {
       headerStyle: {
-        backgroundColor: '#5637DD'
+        backgroundColor: "#5637DD",
       },
-      headerTintColor: '#fff',
+      headerTintColor: "#fff",
       headerTitleStyle: {
-        color: '#fff'
-      }
-    }
+        color: "#fff",
+      },
+    },
   }
 );
 
 const HomeNavigator = createStackNavigator(
   {
-    Home: { screen: Home }
+    Home: { screen: Home },
   },
   {
     navigationOptions: {
       headerStyle: {
-        backgroundColor: '#5637DD'
+        backgroundColor: "#5637DD",
       },
-      headerTintColor: '#fff',
+      headerTintColor: "#fff",
       headerTitleStyle: {
-        color: '#fff'
-      }
-    }
+        color: "#fff",
+      },
+    },
+  }
+);
+//cp
+const AboutNavigator = createStackNavigator(
+  {
+    Home: { screen: About },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+const ContactNavigator = createStackNavigator(
+  {
+    Home: { screen: Contact },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
   }
 );
 
 const MainNavigator = createDrawerNavigator(
   {
     Home: { screen: HomeNavigator },
-    Directory: { screen: DirectoryNavigator }
+    Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator },
   },
   {
-    drawerBackgroundColor: '#CEC8FF'
+    drawerBackgroundColor: "#CEC8FF",
   }
 );
 
 class Main extends Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-      }}>
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
         <MainNavigator />
       </View>
     );
